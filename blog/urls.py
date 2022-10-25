7d442a49f9b6f151f3a165dd3c72db72f8c6836a
Crekan <66014572+Crekan@users.noreply.github.com>
2022-10-26 01:30:36 +0300

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -4,8 +4,6 @@ from .views import *
 urlpatterns = [
     path('blog/', Blog.as_view(), name='blog'),
     path('blog/<slug:categories_blog>/', ShowPost.as_view(), name='detail'),
-    # path('blog/<int:categories_blog>/', com, name='detail'),
     path('categories/<slug:categories_slug>/', ShowCategories.as_view(), name='show_categories'),
     path('search/', Search.as_view(), name='search'),
-    path('like/', like_post, name='like-post'),
 ]
